ae95d222c5ca3778d1aa2665bbab5650fe0df6d4
zhangzewei <453646474@qq.com>
2019-12-16 13:35:42 +0800

    feat: add notification code into story

--- stories/7-Notification.stories.js
+++ stories/7-Notification.stories.js
@@ -34,11 +34,59 @@ const genNoticeBtn = (options) => (
   >{options.message}</Button>
 );
 
-export const NotificationBtns = () => (
-  <div style={{ display: 'flex', flexDirection: 'column' }}>
-    <h1>弹出消息</h1>
-    <div style={{ display: 'flex', justifyContent: 'center' }}>
-      {msgList.map(m => genNoticeBtn(m))}
-    </div>
-  </div>
-);
+class NotificationBtns extends React.Component {
+  componentDidMount() {
+    Prism.highlightAll();
+  }
+
+  render() {
+    const code = `
+    const msgList = [
+      {
+        message: '左上角弹出',
+        placement: 'left-top'
+      },
+      {
+        message: '右上角弹出',
+        placement: 'right-top'
+      },
+      {
+        message: '右上角弹出3s消失',
+        placement: 'right-top',
+        duration: 3000
+      }
+    ];
+    
+    const genNoticeBtn = (options) => (
+      <Button
+        variant="contained"
+        color="primary"
+        style={{ margin: '10px' }}
+        onClick={() => Notification.notice(options)}
+      >{options.message}</Button>
+    );
+
+    const notification = () => (
+      <div style={{ display: 'flex', justifyContent: 'center' }}>
+        {msgList.map(m => genNoticeBtn(m))}
+      </div>
+    );
+    `;
+    return (
+    <div style={{ display: 'flex', flexDirection: 'column' }}>
+      <h1>弹出消息</h1>
+      <div style={{ display: 'flex', justifyContent: 'center' }}>
+        {msgList.map(m => genNoticeBtn(m))}
+      </div>
+      <h3>Code</h3>
+      <pre className="language-javascript">
+        <code className="language-javascript">
+          {code}
+        </code>
+      </pre>
+    </div>  
+    );
+  }
+}
+
+export const notification = () => <NotificationBtns />;
