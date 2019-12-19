a019ed2c9a409b9928aa46832b64b82a32cd4d11
zhangzewei <453646474@qq.com>
2019-12-19 20:53:38 +0800

    refactor: Remove Portal component redundant logic

--- src/components/portal/Portal.tsx
+++ src/components/portal/Portal.tsx
@@ -3,47 +3,48 @@ import ReactDom, { createPortal } from 'react-dom';
 import { CommonComponentProps } from '../../common/Interface';
 
 interface PortalProps extends CommonComponentProps {
-  getContainer?: Function;
-  visible?: boolean;
+  getContainer?: () => HTMLElement;
 }
 
 class Portal extends React.Component<PortalProps> {
-  mountDom: HTMLElement;
-  container: HTMLElement;
-  constructor(props:PortalProps) {
-    super(props);
-    this.mountDom = document.createElement('div');
-    this.container = props.getContainer
-    ? props.getContainer()
-    : window.document.body;
-  }
+  container: HTMLElement | null = null;
 
   componentDidMount() {
-    this.container.appendChild(this.mountDom);
+    this.createContainer();
   }
 
   componentWillUnmount() {
-    if (this.mountDom) {
-      this.container.removeChild(this.mountDom);
-    }
+    this.removeContainer();
   }
 
-  getVisible = () => {
-    if ('visible' in this.props) {
-      return this.props.visible;
+  createContainer = () => {
+    if (this.props.getContainer) {
+      this.container = this.props.getContainer();
+      // Since the update of the component is due to a change in prop or state,
+      // and the dynamic generation of the dom node here does not cause re-render,
+      // manual update is required
+      this.forceUpdate();
     }
-    return true;
-  }
+  };
+
+  removeContainer = () => {
+    if (this.container) {
+      if (this.container.parentNode) {
+        this.container.parentNode.removeChild(this.container);
+      }
+    }
+  };
 
   render() {
     const { children } = this.props;
-    if (this.container && this.getVisible()) {
+
+    if (this.container) {
       if (createPortal) {
-      return createPortal(children, this.mountDom);
+        return createPortal(children, this.container);
       }
-      ReactDom.render(children, this.mountDom);
+      ReactDom.render(children, this.container);
     }
-      return null;
+    return null;
   }
 }
 
