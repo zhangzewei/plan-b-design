05e04be62187658d9255e18904a857e86dc1ea86
zhangzewei <453646474@qq.com>
2019-12-28 22:26:31 +0800

    feat: modal story book

--- stories/9-Dialog.stories.js
+++ stories/9-Dialog.stories.js
@@ -19,7 +19,40 @@ class ModalDemo extends React.Component {
     }
   }
 
+  componentDidMount() {
+    Prism.highlightAll();
+  }
+
   render() {
+    const code = `
+    class ModalDemo extends React.Component {
+      constructor() {
+        super();
+        this.state = {
+          visible: false
+        }
+      }
+    
+      render() {
+        return (
+        <div style={{ display: 'flex', flexDirection: 'column' }}>
+          <h1>弹出Modal</h1>
+          <div style={{ display: 'flex' }}>
+            <Button color="primary" onClick={() => this.setState({ visible: true })}>弹出Modal</Button>
+            <Modal
+              visible={this.state.visible}
+              title="弹窗demo"
+              onOk={() => Notification.message({ type: 'info', message: '弹窗点击确定按钮' })}
+              onClose={() => this.setState({ visible: false })}
+            >
+              <div>modal content</div>
+            </Modal>
+          </div>
+        </div>  
+        );
+      }
+    }
+    `;
     return (
     <div style={{ display: 'flex', flexDirection: 'column' }}>
       <h1>弹出Modal</h1>
@@ -34,6 +67,12 @@ class ModalDemo extends React.Component {
           <div>modal content</div>
         </Modal>
       </div>
+      <h3>Code</h3>
+      <pre className="language-javascript">
+        <code className="language-javascript">
+          {code}
+        </code>
+      </pre>
     </div>  
     );
   }
